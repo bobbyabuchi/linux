12e00e5968e8da97213be59e67c84526c1f32cbc
Nonso Abuchi <bobbyabuchi@gmail.com>
2023-02-10 19:58:28 +0100

    Update basic-bash.sh

--- basic-bash.sh
+++ basic-bash.sh
@@ -3,3 +3,22 @@ sudo passwd root
 
 // create new user
 sudo useradd user_name
+
+// print to screen with 'echo' here we print the type of shell
+echo $SHELL
+
+// show present working directory
+pwd
+
+// create new folder
+mkdir folder_name
+
+// separate commands with semicolon;
+cd folder_name; mkdir folder_name; pwd
+
+// create an entire directory tree, all folders and subfolders
+mkdir -p /tmp/spirit/love/goodness
+
+// remove a folder and contents
+rm -r /tmp/folder_one
+
