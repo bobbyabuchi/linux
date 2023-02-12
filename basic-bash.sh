a9d42408aa79a87ff44f4b8a7e7147951469e594
bobbyabuchi <bobbyabuchi@gmail.com>
2023-02-12 17:32:03 +0100

    linux command updates

--- basic-bash.sh
+++ basic-bash.sh
@@ -52,7 +52,61 @@ wget https://...
 ls /etc/*release* # list OS relate files
 cat /etc/*release* # open OS release related filesss
 
+# --------------- PACKAGE MANAGERS--------------------
+# use 'yum' to install a package and all it's dependencies
+yum install ansible
 
+# list repos
+yum repolist
 
+# remote package location is at
+/etc/yum.repos.d
 
+# need to configure addtional repo?
+ls /etc/yum.repos.d/ # list already existing repos config files
+cat /etc/yum.repos.d/CentOS-Base.repo # see the link to these packages
+
+# see available package details
+yum list ansible
+
+# remove
+yum remove ansible
+
+#  list all available versions and duplicates
+yum --showduplicates list ansible
+
+# install a specific version of package
+yum install ansible-2.4.2.3
+
+# ------------- SERVICES -------------------------
+
+# start a service
+service httpd start # or
+systemctl start httpd
+
+# stop a service
+systemctl stop httpd
+
+# check status
+systemctl status httpd
+
+# enable/start service at startup
+systemctl enable httpd
+
+# configure service not to start at startup
+systemctl disable httpd
+
+# to configure software as a service
+/usr/bin/python3 /opt/code/app.py # a sample Python program
+
+# check if it's running
+curl http://localhost:500
+
+# to configure this as a service
+/etc/systemd/system
+# - create a filename.service in /etc/systemd/system
+# - put the code bellow inside
+# - save
+# [service]
+# ExecStart=/opt/lampp ./manager-linux-x64.run
 
